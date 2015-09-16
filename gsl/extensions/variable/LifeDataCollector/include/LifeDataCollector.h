68c51fa76acfe344415b5a8e42ffe1b0931271c9
James Kozloski <kozloski@us.ibm.com>
2015-09-16 16:02:42 -0400

    Life Data Collection, first pass

--- gsl/extensions/variable/LifeDataCollector/include/LifeDataCollector.h
+++ gsl/extensions/variable/LifeDataCollector/include/LifeDataCollector.h
@@ -4,17 +4,23 @@
 #include "Lens.h"
 #include "CG_LifeDataCollector.h"
 #include <memory>
+#include <fstream>
+#include <iostream>
 
 class LifeDataCollector : public CG_LifeDataCollector
 {
    public:
       void initialize(RNG& rng);
+      void finalize(RNG& rng);
       virtual void dataCollection(Trigger* trigger, NDPairList* ndPairList);
       LifeDataCollector();
       virtual ~LifeDataCollector();
       virtual void duplicate(std::auto_ptr<LifeDataCollector>& dup) const;
       virtual void duplicate(std::auto_ptr<Variable>& dup) const;
       virtual void duplicate(std::auto_ptr<CG_LifeDataCollector>& dup) const;
+
+ private:
+      std::ofstream* file;
 };
 
 #endif
--- gsl/extensions/variable/LifeDataCollector/src/LifeDataCollector.C
+++ gsl/extensions/variable/LifeDataCollector/src/LifeDataCollector.C
@@ -2,22 +2,37 @@
 #include "LifeDataCollector.h"
 #include "CG_LifeDataCollector.h"
 #include <memory>
+#include <fstream>
+#include <sstream>
+#include <iostream>
 
 void LifeDataCollector::initialize(RNG& rng) 
 {
+   // Create the output file...
+  file=new std::ofstream(fileName.c_str());
+}
+
+void LifeDataCollector::finalize(RNG& rng) 
+{
+  file->close();
 }
 
 void LifeDataCollector::dataCollection(Trigger* trigger, NDPairList* ndPairList) 
 {
+  ShallowArray<int*>::iterator iter=vals.begin(), end=vals.end();
+  for (; iter!=end; ++iter) {
+    (*file)<<**iter<<std::endl;
+  }
 }
 
 LifeDataCollector::LifeDataCollector() 
-   : CG_LifeDataCollector()
+  : CG_LifeDataCollector(), file(0)
 {
 }
 
 LifeDataCollector::~LifeDataCollector() 
 {
+  delete file;
 }
 
 void LifeDataCollector::duplicate(std::auto_ptr<LifeDataCollector>& dup) const
