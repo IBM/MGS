672070e3467c946c323300433ae730bb9bc19b7f
James Kozloski <kozloski@us.ibm.com>
2015-09-17 19:06:14 -0400

    LifeDataCollector working

--- gsl/extensions/variable/LifeDataCollector/include/LifeDataCollector.h
+++ gsl/extensions/variable/LifeDataCollector/include/LifeDataCollector.h
@@ -13,6 +13,7 @@ class LifeDataCollector : public CG_LifeDataCollector
       void initialize(RNG& rng);
       void finalize(RNG& rng);
       virtual void dataCollection(Trigger* trigger, NDPairList* ndPairList);
+      virtual void getNodeIndices(const String& CG_direction, const String& CG_component, NodeDescriptor* CG_node, Edge* CG_edge, VariableDescriptor* CG_variable, Constant* CG_constant, CG_LifeDataCollectorInAttrPSet* CG_inAttrPset, CG_LifeDataCollectorOutAttrPSet* CG_outAttrPset);
       LifeDataCollector();
       virtual ~LifeDataCollector();
       virtual void duplicate(std::auto_ptr<LifeDataCollector>& dup) const;
