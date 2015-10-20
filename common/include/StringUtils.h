6780055eef43f821f2d52c50825dd52b282b4ac7
James Kozloski <kozloski@us.ibm.com>
2015-10-20 10:46:31 -0400

    added common to master

---
+++ common/include/StringUtils.h
@@ -0,0 +1,13 @@
+#ifndef __STRINGUTILS_H__
+#define __STRINGUTILS_H__
+
+#include <string>
+#include <vector>
+
+class StringUtils
+{
+	public:
+		static void Tokenize(const std::string& str, std::vector<std::string>& tokens, const std::string& delimiters = " ");
+};
+
+#endif
