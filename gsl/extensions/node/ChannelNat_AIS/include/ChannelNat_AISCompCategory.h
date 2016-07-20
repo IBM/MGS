f3bc4dde65bcc3ffc38165308557a1a88bb2b8ad
Tuan M. Hoang Trong <tmhoangt@us.ibm.com>
2016-07-20 17:48:34 -0400

    fix: Nat_AIS and CaHVA

--- gsl/extensions/node/ChannelNat_AIS/include/ChannelNat_AISCompCategory.h
+++ gsl/extensions/node/ChannelNat_AIS/include/ChannelNat_AISCompCategory.h
@@ -3,14 +3,17 @@
 
 #include "Lens.h"
 #include "CG_ChannelNat_AISCompCategory.h"
+#include "CountableModel.h"
 
 class NDPairList;
 
-class ChannelNat_AISCompCategory : public CG_ChannelNat_AISCompCategory
+class ChannelNat_AISCompCategory : public CG_ChannelNat_AISCompCategory,
+                               public CountableModel
 {
    public:
       ChannelNat_AISCompCategory(Simulation& sim, const std::string& modelName, const NDPairList& ndpList);
       void computeE(RNG& rng);
+      void count();
 };
 
 #endif
