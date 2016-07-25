03f5a0ad0a6e2c39567445b77c22d47094fab337
Tuan M. Hoang Trong <tmhoangt@us.ibm.com>
2016-07-25 11:20:52 -0400

    fix: bug in TissueFunctor (NTS_MGS_HYBRID) update:   + move params into neurons_traub51   + BranchSolver.mdl: remove check for MAX_COMPUTE_ORDER (to avoid redundant recompile when we change the value)

--- graphs/FSI-MSN/changemorph.sh
+++ graphs/FSI-MSN/changemorph.sh
@@ -1,34 +1,40 @@
 #!/bin/bash
 ####
 # This script was written to enable quicly switching from one morphology to another
+#  and one model to another
 # author: Tuan M. Hoang Trong (IBM, @2016)
 #
+rm_if_link(){ [ ! -L "$1" ] || rm -v "$1"; }
+clean_all() {
+  rm_if_link neurons
+  rm_if_link params
+  rm_if_link connect_recording_model.gsl
+  rm_if_link recording_model.gsl
+  rm_if_link connect_stimulus_model.gsl
+  rm_if_link stimulus_model.gsl
+  rm_if_link model.gsl
+}
+
+ModelFolder=systems
+
 if [ "$#" == "0" ]; then
-  echo "$0 <prefix> "
-  echo "    <prefix> is the prefix of the folder where (1) morphology, (2) params, (3) recording/stimulus sites for that morph are stored"
+  echo "$0 <morph_suffix> "
+  echo "    <suffix> is the suffix of the folder where (1) morphology, (2) params, (3) recording/stimulus sites for that morph are stored"
   echo "  e.g. traub51, traub3, traub1"
   echo "NOTE: $0 clean "
   echo "   to clean the symbolic links"
+  exit
 fi
 if [ "$1" == "clean" ]; then
-rm neurons
-rm params
-rm connect_recording_model.gsl
-rm recording_model.gsl
-rm connect_stimulus_model.gsl
-rm stimulus_model.gsl
+  clean_all
 else
-#NOTE: $1  = suffix (e.g. hay2, hay1, unknown)
-rm neurons
-ln -s neurons_$1 neurons
-rm params
-ln -s params_$1 params
-rm connect_recording_model.gsl
-ln -s neurons/connect_recording_model_$1.gsl connect_recording_model.gsl
-rm recording_model.gsl
-ln -s neurons/recording_model_$1.gsl recording_model.gsl
-rm connect_stimulus_model.gsl
-ln -s neurons/connect_stimulus_model_$1.gsl  connect_stimulus_model.gsl
-rm stimulus_model.gsl
-ln -s neurons/stimulus_model_$1.gsl stimulus_model.gsl
+  #NOTE: $1  = suffix (e.g. hay2, hay1, unknown)
+  clean_all
+  ln -s neurons_$1 neurons
+
+  ln -s neurons_$1/params params
+  ln -s neurons/connect_recording_model_$1.gsl connect_recording_model.gsl
+  ln -s neurons/recording_model_$1.gsl recording_model.gsl
+  ln -s neurons/connect_stimulus_model_$1.gsl  connect_stimulus_model.gsl
+  ln -s neurons/stimulus_model_$1.gsl stimulus_model.gsl
 fi
