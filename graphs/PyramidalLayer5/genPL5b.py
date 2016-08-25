46bdb45dc227720770afd793ceb62e30f0523564
Tuan M. Hoang Trong <tmhoangt@us.ibm.com>
2016-08-24 23:27:35 -0400

    update run script, and morphology code

--- graphs/PyramidalLayer5/genPL5b.py
+++ graphs/PyramidalLayer5/genPL5b.py
@@ -5,8 +5,14 @@ from genSpines import SomeClass
 ##############
 targetDir = "neurons"
 #neuron = SomeClass(targetDir +"/neuron.swc")
-neuron = SomeClass(targetDir +"/hay2_0.developed.swc")
-neuron.genPL5b()
+#neuron = SomeClass(targetDir +"/hay2_0.developed.swc")
+#neuron.genPL5b()
+#neuron = SomeClass(targetDir +"/hay1.swc_original_revisedSoma.swc")
+neuron = SomeClass(targetDir +"/hay1.swc_original_revisedSoma.swc_changedBranch.swc")
+neuron.genPL5b_hay1()
+dist=0.0 #[um]
+neuron = SomeClass(targetDir +"/hay1.swc_original_revisedSoma.swc_changedBranch.swc_scaledBasal.swc")
+neuron.removeBasalBranch(dist)
 
 ##############
 # convert the branch supposed to be apical to apical
@@ -16,6 +22,6 @@ neuron.genPL5b()
 #neuron = SomeClass(targetDir +"/hay1.swc")
 ## convert points from line 3442 to apical (4)
 ##startIndex = [3442]# hay2
-#startIndex = [1663] #hay1
-#newBranch=4   #apical-den
+startIndex = [1663] #hay1
+newBranch=4   #apical-den
 #neuron.convertBranch(startIndex, newBranch)
