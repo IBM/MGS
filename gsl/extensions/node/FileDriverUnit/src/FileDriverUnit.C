5b4c8ed6a4542b81fa2bc38298c828ce4ad2a4d1
James Humble <jmhumble@us.ibm.com>
2017-08-05 23:59:31 -0400

    Simplified FileDriverUnit.
    Instead of having a copy of the outputs from shared to 'individual',
    there is now just a pointer to the shared. Saves on a phase.

--- gsl/extensions/node/FileDriverUnit/src/FileDriverUnit.C
+++ gsl/extensions/node/FileDriverUnit/src/FileDriverUnit.C
@@ -30,13 +30,8 @@ void FileDriverUnit::initialize(RNG& rng)
   if (SHD.op_random)
     channel = irandom(0, SHD.n_channels - 1, rng);
   else
-    channel = getGlobalIndex() % SHD.n_channels;  
-}
-
-void FileDriverUnit::updateOutput(RNG& rng)
-{
-  if (ITER % SHD.period == 0)  
-    output = SHD.input[channel] * SHD.scale;
+    channel = getGlobalIndex() % SHD.n_channels;
+  output = const_cast<double*>(&(SHD.input[channel]));
 }
 
 void FileDriverUnit::setIndices(const String& CG_direction, const String& CG_component, NodeDescriptor* CG_node, Edge* CG_edge, VariableDescriptor* CG_variable, Constant* CG_constant, CG_FileDriverUnitInAttrPSet* CG_inAttrPset, CG_FileDriverUnitOutAttrPSet* CG_outAttrPset) 
