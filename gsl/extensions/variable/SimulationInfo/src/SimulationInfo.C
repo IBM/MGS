9085f2d82c43e638ed4c21d09232350b75ec20ef
Tuan M. Hoang Trong <tmhoangt@us.ibm.com>
2017-08-17 15:31:06 -0400

    add Mahon model  GSL + params

--- gsl/extensions/variable/SimulationInfo/src/SimulationInfo.C
+++ gsl/extensions/variable/SimulationInfo/src/SimulationInfo.C
@@ -10,7 +10,13 @@ void SimulationInfo::initialize(RNG& rng)
       std::cerr << "ERROR: Please connect deltaT to " << typeid(*this).name() << std::endl;
    }
    assert(deltaT);
+   if (recordIntervalInTime <= 0 and recordIntervalInIterations <= 0)
+   {
+      std::cerr << "ERROR: Please define recordIntervalInTime > 0  or recordIntervalInIterations > 0 for " << typeid(*this).name() << std::endl;
+   }
+   //at least one must be used
    assert(recordIntervalInTime > 0 || recordIntervalInIterations > 0);
+
    if (recordIntervalInTime > 0)
       recordIntervalInIterations = (int)(recordIntervalInTime / (*deltaT));
    else if (recordIntervalInIterations > 0)
@@ -26,8 +32,14 @@ void SimulationInfo::initialize(RNG& rng)
 
 void SimulationInfo::calculateInfo(RNG& rng) 
 {
-   currentTime = (*deltaT) * getSimulation().getIteration();
-   iterationCount  = getSimulation().getIteration() - 1;
+   //currentTime = (*deltaT) * getSimulation().getIteration();
+   currentTime += (*deltaT); //by doing this - we can configure a negative time and use this to 
+                             // set-up a way to run-until-steady-state before any data I/O
+                             // is performed which use 0-based time as criteria
+                             // NOTE: Not all system has steady-states (e.g. those with oscillation)
+                             // so this is not always useful
+   // * getSimulation().getIteration();
+   iterationCount  = getSimulation().getIteration() - 1; //zero-based for RuntimePhase
 }
 
 SimulationInfo::SimulationInfo() 
