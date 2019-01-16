38cf13253a55483c909b88b0199b1a8e805771bd
Tuan M. Hoang Trong <tmhoangt@us.ibm.com>
2019-01-15 22:13:13 -0500

    revise build to accommodate more complicated build scenario for client, e.g. njit complete add MahonUnit and SwitchInput make classes in NumIntNoPhase.h/.C with 'NoPhase' suffix

--- gsl/utils/std/include/NumIntNoPhase.h
+++ gsl/utils/std/include/NumIntNoPhase.h
@@ -20,50 +20,50 @@
 
 #include "ShallowArray.h"
 
-//class numInt
-//{
-// protected :
-//
-//  virtual void initializeIterator(int, double) = 0;
-//  virtual void callIterate(ShallowArray< double > &) = 0;
-//  virtual void derivs(const ShallowArray< double > &, ShallowArray< double > &) = 0;
-//
-//};
-//
-//
-//class EU : public numInt
-//{
-//protected:
-//  
-//  void initializeIterator(int, double);
-//  void callIterate(ShallowArray< double > &);
-//
-// private:
-//
-//  ShallowArray< double > dx1;
-//  ShallowArray< double >::iterator i1, i2;
-//  double dT;
-//
-//};
-//
-//
-//
-//class RK4 : public numInt
-//{
-//  // public:
-//  //  RK4(){} 
-//  
-//  
-//  protected:
-//  
-//  void initializeIterator(int, double);
-//  void callIterate(ShallowArray< double > &);
-//
-// private:
-//
-//  ShallowArray< double > dx1, dx2, dx3, x1;
-//  ShallowArray< double >::iterator i1, i2, i3, i4;
-//  double dT, dT2, dT6;
-//};
+class numIntNoPhase
+{
+ protected :
+
+  virtual void initializeIterator(int, double) = 0;
+  virtual void callIterate(ShallowArray< double > &) = 0;
+  virtual void derivs(const ShallowArray< double > &, ShallowArray< double > &) = 0;
+
+};
+
+
+class EUNoPhase : public numIntNoPhase
+{
+protected:
+  
+  void initializeIterator(int, double);
+  void callIterate(ShallowArray< double > &);
+
+ private:
+
+  ShallowArray< double > dx1;
+  ShallowArray< double >::iterator i1, i2;
+  double dT;
+
+};
+
+
+
+class RK4NoPhase : public numIntNoPhase
+{
+  // public:
+  //  RK4(){} 
+  
+  
+  protected:
+  
+  void initializeIterator(int, double);
+  void callIterate(ShallowArray< double > &);
+
+ private:
+
+  ShallowArray< double > dx1, dx2, dx3, x1;
+  ShallowArray< double >::iterator i1, i2, i3, i4;
+  double dT, dT2, dT6;
+};
 
 #endif
--- gsl/utils/std/src/NumIntNoPhase.C
+++ gsl/utils/std/src/NumIntNoPhase.C
@@ -22,60 +22,56 @@
 #include <iostream>
 #include <math.h>
 
-//void EU::initializeIterator(int numvars_, double dT_)
-//{
-//  dT = dT_; 
-//  dx1.increaseSizeTo(numvars_);
-//}
-//
-//
-//void EU::callIterate(ShallowArray< double > & x)
-//{
-//  derivs(x,dx1); 
-//
-//  for (i1=dx1.begin(),i2=x.begin();i1!=dx1.end();i1++,i2++)
-//   *i2 = *i2 + dT**i1;
-//
-//}
-//
-//
-//
-//void RK4::initializeIterator(int numvars_, double dT_)
-//   {
-//     dT = dT_; dT2=dT_/2.0; dT6 = dT_/6.0;
-//     dx1.increaseSizeTo(numvars_);
-//     dx2.increaseSizeTo(numvars_);
-//     dx3.increaseSizeTo(numvars_);
-//     x1.increaseSizeTo(numvars_);  
-//   }
-//
-//
-//void RK4::callIterate(ShallowArray< double > & x)
-//{
-//
-//  derivs(x,dx1); 
-//
-//  
-//  for (i1=x1.begin(),i2=x.begin(),i3=dx1.begin();i1!=x1.end();i1++,i2++,i3++) 
-//    *i1 = *i2 + dT2**i3;
-//
-//  
-//  derivs(x1,dx2);
-//
-//  
-//  for (i1=x1.begin(),i2=x.begin(),i3=dx2.begin();i1!=x1.end();i1++,i2++,i3++) 
-//    *i1 = *i2 + dT2**i3;
-//  derivs(x1,dx3);
-//  for (i1=x1.begin(),i2=x.begin(),i3=dx3.begin(),i4=dx2.begin();i1!=x1.end();i1++,i2++,i3++,i4++)
-//    { 
-//    *i1 = *i2 + dT**i3;
-//    *i3 += *i4;
-//    }
-//  
-//  derivs(x1,dx2);
-//  for (i1=dx1.begin(),i2=x.begin(),i3=dx3.begin(),i4=dx2.begin();i1!=dx1.end();i1++,i2++,i3++,i4++)
-//   *i2 = *i2 + dT6*(*i1 + *i4 + 2.0**i3);
-//
-//  
-//
-//}
+void EUNoPhase::initializeIterator(int numvars_, double dT_)
+{
+  dT = dT_; 
+  dx1.increaseSizeTo(numvars_);
+}
+
+
+void EUNoPhase::callIterate(ShallowArray< double > & x)
+{
+  derivs(x,dx1); 
+
+  for (i1=dx1.begin(),i2=x.begin();i1!=dx1.end();i1++,i2++)
+   *i2 = *i2 + dT**i1;
+
+}
+
+
+
+void RK4NoPhase::initializeIterator(int numvars_, double dT_)
+{
+  dT = dT_; dT2=dT_/2.0; dT6 = dT_/6.0;
+  dx1.increaseSizeTo(numvars_);
+  dx2.increaseSizeTo(numvars_);
+  dx3.increaseSizeTo(numvars_);
+  x1.increaseSizeTo(numvars_);  
+}
+
+
+void RK4NoPhase::callIterate(ShallowArray< double > & x)
+{
+  derivs(x,dx1); 
+
+  
+  for (i1=x1.begin(),i2=x.begin(),i3=dx1.begin();i1!=x1.end();i1++,i2++,i3++) 
+    *i1 = *i2 + dT2**i3;
+
+  
+  derivs(x1,dx2);
+
+  
+  for (i1=x1.begin(),i2=x.begin(),i3=dx2.begin();i1!=x1.end();i1++,i2++,i3++) 
+    *i1 = *i2 + dT2**i3;
+  derivs(x1,dx3);
+  for (i1=x1.begin(),i2=x.begin(),i3=dx3.begin(),i4=dx2.begin();i1!=x1.end();i1++,i2++,i3++,i4++)
+    { 
+    *i1 = *i2 + dT**i3;
+    *i3 += *i4;
+    }
+  
+  derivs(x1,dx2);
+  for (i1=dx1.begin(),i2=x.begin(),i3=dx3.begin(),i4=dx2.begin();i1!=dx1.end();i1++,i2++,i3++,i4++)
+   *i2 = *i2 + dT6*(*i1 + *i4 + 2.0**i3);
+}
