3878441de133ea90de3b83b28394532761849eac
Tuan M. Hoang Trong <tmhoangt@us.ibm.com>
2019-07-16 14:39:39 -0400

    add code to download and install ompi + mpich test .travis.yml

---
+++ get_ompi.sh
@@ -0,0 +1,19 @@
+if [ -f mpich/lib/libmpi.so ]; then
+  echo "libmpi.so found -- nothing to build."
+else
+  FILENAME=openmpi-3.1.4
+  echo "Downloading openMPI source."
+  wget https://download.open-mpi.org/release/open-mpi/v3.1/${FILENAME}.tar.gz
+  tar xfz ${FILENAME}.tar.gz
+  rm ${FILENAME}.tar.gz
+  echo "configuring and building openMPI."
+  cd ${FILENAME}
+  ./configure \
+    --enable-mpi-cxx \
+    --prefix=`pwd`/../mpich \
+    --with-cuda
+  make -j4
+  make install
+  cd -
+  rm -rf ${FILENAME}
+fi
