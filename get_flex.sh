bc1470a4d0d1305e1453eecfeff263c0b6443be7
Tuan M. Hoang Trong <tmhoangt@us.ibm.com>
2019-07-16 19:17:44 -0400

    test travis.yml

---
+++ get_flex.sh
@@ -0,0 +1,8 @@
+wget "https://github.com/westes/flex/releases/download/v2.6.4/flex-2.6.4.tar.gz"
+tar -xvf flex-2.6.4.tar.gz
+cd flex-2.6.4
+./configure --prefix=`pwd`/../flex
+make -j10
+make install
+cd -
+rm -rf flex-2.6.4
