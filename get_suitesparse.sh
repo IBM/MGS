6873eda47b19a4113b2a8f6b135b65e221397e9d
Tuan M. Hoang Trong <tmhoangt@us.ibm.com>
2019-07-19 09:56:50 -0400

    revise compile only needed components in SuiteSparse

--- get_suitesparse.sh
+++ get_suitesparse.sh
@@ -1,11 +1,14 @@
 FILENAME=SuiteSparse-5.4.0
 DIRNAME=SuiteSparse
+if [ ! -f $FILENAME.tar.gz ]; then
 wget "http://faculty.cse.tamu.edu/davis/SuiteSparse/$FILENAME.tar.gz"
+fi
 
 tar -xvf ${FILENAME}.tar.gz > /dev/null
 cd ${DIRNAME}
-./configure --prefix=$TRAVIS_BUILD_DIR/suitesparse
+#./configure --prefix=$TRAVIS_BUILD_DIR/suitesparse
+cd CXSparse
 make -j10
 make install
 cd -
-rm -rf $DIRNAME
+#rm -rf $DIRNAME
